ec25e7e0030e87289a61be4a82abbe072c286c7b
Gian Paolo Mulingbayan <23-77160@g.batstate-u.edu.ph>
2025-11-29 20:14:43 +0800

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
-            CategorySeeder::class,
+            CustomerSeeder::class,
 
         ]);
     }
