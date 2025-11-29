ed0ab528c70bf10ab871e633c1a2c86bce99ddd4
Gian Paolo Mulingbayan <23-77160@g.batstate-u.edu.ph>
2025-11-29 20:59:56 +0800

    wip

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Faker\Factory
 
 class UserFactory extends Factory
 {
